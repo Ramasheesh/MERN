1a2821ada85c676540b850a98212c18244e142f2
Ramasheesh <rch231198@gmail.com>
2024-08-12 21:33:16 +0530

    done

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 4040;
 const dbConnect = require("./connection/Db.connection");
 const Message = require("./models/contacts.js");
 
